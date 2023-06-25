1e5e2622ad95750e2f8a70b494a56caef1fe92fd
Ogabek <famifigma@gmail.com>
2023-06-25 14:40:38 +0500

    javascr

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+
+var modal = prompt("Ismingizni kiriting")
+console.log("Mening ismim " + modal );
+var modal= +prompt("Yoshingizni kiriting")
+console.log("Sizning yoshingiz " + modal + " da ");
+var num = +prompt("6 + 3 = ?")
+
+console.log("Misol 1: " + " 6 + 3 = 9 " + "Sizning javobingiz " + num );
+
+var num = +prompt("20 - 5 = ?")
+
+console.log("Misol 2: " + " 20 - 5 = 15 " + "Sizning javobingiz " + num );
+
+var num = +prompt("15 * 2 = ?")
+
+console.log("Misol 3: " + " 15 * 2 = 30 " + "Sizning javobingiz " + num );
+
+var num = +prompt("16 / 4 = ?")
+
+console.log("Misol 4: " + " 16 / 4 = 4 " + "Sizning javobingiz " + num );
+
+var num = +prompt("18 % 3 = ?")
+
+console.log("Misol 5: " + "18 % 3 = 1 " + "Sizning javobingiz " + num );
+
